7cd8a67c69fa4a7fec7b162d54adacb9ff7fa83e
bifft2 <bifft95@gmail.com>
2023-01-25 17:42:17 -0600

    initial commit

---
+++ starter-app/firstapp/__init__.py
@@ -0,0 +1,53 @@
+print("hello world")
+
+
+# this is a single line comment
+
+"""
+Task:
+- ask the user for one number
+- ask the user for another number
+- print the sum of their numbers
+"""
+number_1 = input("Enter a number: ")
+number_2 = input("Enter another number: ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import random
+
+# from flask import Flask, render_template
+
+# def create_app(test_config=None):
+#     # create and configure the app
+#     app = Flask(__name__, instance_relative_config=True)
+
+#     # a simple page that says hello
+#     @app.route('/encouragement')
+#     def hello():
+#         encouragements = ["You're incredible!", "You got this.", "Sending good vibes your way :)", "You can do it!"]
+#         encouragement = random.choice(encouragements)
+#         return render_template('index.html', encouragement=encouragement)
+
+
+#     return app
