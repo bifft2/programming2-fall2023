891f4a83a9714fa04d34703610a9cbb7af5e05c4
bifft2 <bifft95@gmail.com>
2023-02-09 11:39:13 -0600

    adding recipe book folder

--- starter-app/firstapp/__init__.py
+++ starter-app/firstapp/__init__.py
@@ -1,39 +1,16 @@
+import random
 
+from flask import Flask, render_template
 
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
 
+    # a simple page that says hello
+    @app.route('/encouragement')
+    def hello():
+        encouragements = ["You're incredible!", "You got this.", "Sending good vibes your way :)", "You can do it!"]
+        encouragement = random.choice(encouragements)
+        return render_template('index.html', encouragement=encouragement)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import random
-
-# from flask import Flask, render_template
-
-# def create_app(test_config=None):
-#     # create and configure the app
-#     app = Flask(__name__, instance_relative_config=True)
-
-#     # a simple page that says hello
-#     @app.route('/encouragement')
-#     def hello():
-#         encouragements = ["You're incredible!", "You got this.", "Sending good vibes your way :)", "You can do it!"]
-#         encouragement = random.choice(encouragements)
-#         return render_template('index.html', encouragement=encouragement)
-
-
-#     return app
+    return app
